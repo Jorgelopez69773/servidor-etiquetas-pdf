d1c69c3405b41bfbd490b2613c36624f29c52470
Jorge Lopez <84600587+Jorgelopez69773@users.noreply.github.com>
2025-12-03 17:53:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.post("/zpl-pdf", async (req, res) => {
     for (const id of id_bombas) {
       let bomba=id-1;
       let cod = `000000${bomba}`;
-      let cod2=cod.slice(-6)
+      let cod2=cod.slice(-6);
+      let tipo=tipo[0];
        
       // Generar tipo, potencia y caudal en el servidor
       const tipo_motor = tipo.startsWith("Bomba 0,75HP") ? "0,75HP" : "0,50HP";
